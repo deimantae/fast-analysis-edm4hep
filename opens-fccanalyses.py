f25d6896bc878de3efb7b4133a761daa52e6b786
Deimantė Juknevičiūtė <deimante.jukneviciute@ff.stud.vu.lt>
2026-07-17 16:28:08 +0200

    Update opens-fccanalyses.py
    The name of the file that stores the list of desired variables is now a command line argument

--- opens-fccanalyses.py
+++ opens-fccanalyses.py
@@ -1,25 +1,55 @@
 import yaml
+import uproot
 from pathlib import Path
+from argparse import ArgumentParser
 
-#Define the operations on the dataframe
+'''
+Test mode:
+    $ fccanalysis run examples/FCCee/tutorials/opens-fccanalyses.py \
+        --test -- --parameters-file file_variables.yaml
+Full analysis:
+    $ fccanalysis run examples/FCCee/tutorials/opens-fccanalyses.py \
+        -- --parameters-file file_variables.yaml
+'''
+
+# Define the operations on the dataframe
 class Analysis:
     
     def __init__(self, cmdline_args):
-        self.process_list = {}
-        self.input_dir = '/eos/experiment/fcc/hh/tutorials/'\
-            'edm4hep_tutorial_data/'
+        # Parse additional arguments
+        # All command line arguments are provided in the `cmdline_arg`
+        # dictionary and arguments after "--" are stored under "remaining" key.
+        parser = ArgumentParser(
+            description="Additional analysis arguments",
+            usage="Provided after '--'")
+        parser.add_argument("--parameters-file", required=True, type=str,
+                            help="YAML file containing the variables.")
+        self.ana_args, _ = parser.parse_known_args(cmdline_args["remaining"])
+
+        # Run over the full statistics and save it to one output file named
+        # <outputDir>/<process_name>.root
+        self.process_list = {
+                'p8_ee_ZZ_ecm240': {'fraction': 0.5}, #  statistics percent
+                'p8_ee_WW_ecm240': {'fraction': 0.25},
+                'p8_ee_ZH_ecm240': {'fraction': 0.2,}
+        }
+        
+        self.input_dir = "/eos/experiment/fcc/hh/tutorials/"\
+            "edm4hep_tutorial_data/"
         self.output_dir = "outputs/opens-fccanalyses"
-        self.test_file = 'https://fccsw.web.cern.ch/fccsw/analysis/' \
-                         'test-samples/edm4hep099/p8_ee_WW_ecm240_edm4hep.root'
+        self.test_file = "https://fccsw.web.cern.ch/fccsw/analysis/" \
+                         "test-samples/edm4hep099/p8_ee_WW_ecm240_edm4hep.root"
         
-        #Load variables from the YAML configuration file
-        config_file = Path(__file__).parent / "jet_variables.yaml"
+        # Load variables from the YAML configuration file
+        config_file = Path(__file__).parent / self.ana_args.parameters_file
+        #transform to rntuple
+        #then coffea-withedm4hep code
         
-        #Store YAML contents as a dictionary
+        # Store YAML contents as a dictionary
         with open(config_file, "r") as file:
             self.variables = yaml.safe_load(file)
 
-    #Return the transformed RDataFrame
+    # Return the transformed RDataFrame
     def analyzers(self, dframe):
         #Create new branches for every variable defined in the YAML file
         for branch_name, branch_info in self.variables.items():
@@ -30,11 +60,14 @@ class Analysis:
         
         return dframe
     
-    #Return the list of branches to save
+    # Return the list of branches to save 
+    
+    # TTree
     def output(self):
         return list(self.variables.keys())
     
-    #rntuple?
-    #mass charge Jet.goodnessOfPID #now use these, we will have one that is computing and some in the file
-    
-    
+    '''
+    # RNTuple
+    with uproot.recreate("jets_coffea.root") as output_file:
+        output_file.mkrntuple("Events", jet_output)    
+'''
