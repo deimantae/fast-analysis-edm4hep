ae1f11a2c26094e679bfaab2831249298342fbc5
Deimante Jukneviciute <djuknevi@lxplus919.cern.ch>
2026-07-24 15:27:58 +0200

    Adapt Coffea FCC Analyses script to a new more generic .yaml file format and change design of histograms

--- Coffea-FCCAnalyses/evaluation_histograms.py
+++ Coffea-FCCAnalyses/evaluation_histograms.py
@@ -1,6 +1,7 @@
 import awkward as ak
 import uproot
 import hist
+import mplhep as hep
 from coffea import util
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
@@ -16,10 +17,6 @@ with uproot.open("output.root") as output_file:
     output_histograms = {}
 
     for branch in rntuple.keys():
-        # Filter internal RNTuple storage fields
-        if "._" in branch:
-            continue
-
         values = rntuple[branch].array()
 
         output_histograms[branch] = (
@@ -28,32 +25,105 @@ with uproot.open("output.root") as output_file:
             .Double()
             .fill(ak.ravel(values))
         )
+        
+# Plot style
+hep.style.use("ROOT")
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.serif": ["Times New Roman", "DejaVu Serif"],
+    "font.size": 14,
+    "mathtext.fontset": "stix",
+})
 
 # Compare input and output histograms
 with PdfPages("histogram_comparison.pdf") as pdf:
     for branch in output_histograms:
-        fig, ax = plt.subplots(figsize=(6, 4))
+        input_hist = input_histograms[branch]
+        output_hist = output_histograms[branch]
+        
+        fig, (ax, ax_difference) = plt.subplots(
+            2,
+            1,
+            figsize=(6, 6),
+            sharex=True,
+            gridspec_kw={"height_ratios": [3, 1],
+                         "hspace": 0.08}
+            )
 
-        input_histograms[branch].plot1d(
+        input_hist.plot1d(
             ax=ax,
-            label="Input",
-            color="blue",
-            linewidth=2,
+            label=r"$h_1$",
+            color="navy",
+            linewidth=1.5
         )
 
-        output_histograms[branch].plot1d(
+        output_hist.plot1d(
             ax=ax,
-            label="Output",
+            label=r"$h_2$",
             color="lightsteelblue",
-            linewidth=2,
-            linestyle="--",
+            linewidth=1.5,
+            linestyle="--"
         )
+    
+        # Difference panel
+        difference = input_hist.values() - output_hist.values()
+        
+        ax_difference.axhline(
+            0,
+            color="lightsteelblue",
+            linestyle="--",
+            linewidth=1
+            )
 
-        ax.set_title(branch)
-        ax.set_xlabel(branch)
+        ax_difference.plot(
+            input_hist.axes[0].centers,
+            difference,
+            linestyle="none",
+            marker="o",
+            color="navy",
+            markersize=3
+            )
+        
+        ax.legend(frameon=False, loc="upper right")
+        
+        # Top panel 
         ax.set_ylabel("Entries")
-        ax.legend()
-        fig.tight_layout()
+        ax.tick_params(labelbottom=False)
+        ax.xaxis.label.set_visible(False)
+        
+        # Bottom panel
+        ax_difference.set_ylabel(r"$h_1-h_2$")
+        ax_difference.set_xlabel(branch)
+        
+        # Center axis labels
+        ax.yaxis.set_label_coords(-0.08, 0.5)
+        ax_difference.yaxis.set_label_coords(-0.08, 0.7)
+        
+        # Ticks pointing inward
+        for axis in (ax, ax_difference):
+            for spine in axis.spines.values():
+                spine.set_linewidth(1.0)
+            
+            axis.tick_params(
+                direction="in",
+                which="major",
+                length=4,
+                width=0.8,
+                top=True,
+                right=True
+                )
+            
+            axis.tick_params(
+                direction="in",
+                which="minor",
+                length=2,
+                width=0.6,
+                top=True,
+                right=True
+                )
+            
+            axis.minorticks_on()
+        
         pdf.savefig(fig)
         plt.close(fig)
 
