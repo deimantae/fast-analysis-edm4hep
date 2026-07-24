4257babc293c031a295d7539926e0a1b1b2e2d18
Deimante Jukneviciute <deimante.jukneviciute@ff.stud.vu.lt>
2026-07-24 15:27:58 +0200

    Adapt Coffea FCC Analyses script to a new more generic .yaml file format and change design of histograms

--- Coffea-FCCAnalyses/opens-coffea-fccanalyses.py
+++ Coffea-FCCAnalyses/opens-coffea-fccanalyses.py
@@ -8,15 +8,15 @@ from argparse import ArgumentParser
 from coffea import util
 
 # Example FCC winter 2023 sample
-'''
+
 fname = (
     "root://eospublic.cern.ch//eos/experiment/fcc/ee/generation/"
     "DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_Hbb_ecm240/"
     "events_159112833.root"
 )
-'''
 
-fname = "events_159112833.root"
+
+#fname = "events_159112833.root"
 
 parser = ArgumentParser(description="Additional analysis arguments")
 parser.add_argument("--parameters-file", required=True, type=str,
@@ -40,21 +40,39 @@ events = NanoEventsFactory.from_root(
 variables_output = {}
 histograms = {}
 
-for branch, branch_info in variables.items():
-    values = eval(branch_info["line"])
-    variables_output[branch] = values
+for collection_name, variable_list in variables.items():
+    if variable_list is None:
+        continue
 
-    histogram = (
-        hist.Hist.new
-        .Reg(50, 0, 150)
-        .Double()
-        .fill(ak.ravel(values))
-    )
-    histograms[branch] = histogram
+    collection = getattr(events, collection_name)
+
+    for variable in variable_list:
+        # String variable
+        if isinstance(variable, str):
+            variable_name = variable
+            values = getattr(collection, variable_name)
+        # Mathematical operation variable
+        else:
+            for variable_name, expression in variable.items():
+                for field in collection.fields:
+                    expression = expression.replace(field,
+                                                    f"collection.{field}")
+                values = eval(expression)
+
+        output_name = f"{collection_name}_{variable_name}"
+        variables_output[output_name] = values
+        
+        histogram = (
+            hist.Hist.new
+            .Reg(50, 0, 150)
+            .Double()
+            .fill(ak.ravel(values))
+            )
+        
+        histograms[output_name] = histogram
 
 output_array = ak.zip(variables_output, depth_limit=1)
 util.save(histograms, "input_histograms.coffea")
-
 print("Saved input histogram objects to input_histograms.coffea")
 
 # Create RNTuple output file 
